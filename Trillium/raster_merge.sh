9a892030f84d6a8667ddbaad4de78dbf17aa90e2
Alexander J. F. Martin <128729369+arbmarta@users.noreply.github.com>
2025-08-11 16:03:39 -0500

    Update raster_merge.sh

--- Trillium/raster_merge.sh
+++ Trillium/raster_merge.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=merge_eth
-#SBATCH --time=00:30:00
-#SBATCH --mem=16G
-#SBATCH --cpus-per-task=2
-#SBATCH --output=/scratch/arbmarta/merge_eth_%j.out
-#SBATCH --error=/scratch/arbmarta/merge_eth_%j.err
+#SBATCH --time=00:15:00
+#SBATCH --mem=8G
+#SBATCH --cpus-per-task=1
 
-# Limit implicit threading
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-export OPENBLAS_NUM_THREADS=$SLURM_CPUS_PER_TASK
-export MKL_NUM_THREADS=$SLURM_CPUS_PER_TASK
-export NUMEXPR_NUM_THREADS=$SLURM_CPUS_PER_TASK
-
-# Activate your env
 source /home/arbmarta/.virtualenvs/myenv/bin/activate
-
-# Run your script
 python /scratch/arbmarta/raster_merge.py
