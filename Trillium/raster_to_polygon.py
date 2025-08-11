f842fe4bcf56bd851be769073820adcad68480c7
Alexander J. F. Martin <128729369+arbmarta@users.noreply.github.com>
2025-08-11 14:20:33 -0500

    Update raster_to_polygon.py

--- Trillium/raster_to_polygon.py
+++ Trillium/raster_to_polygon.py
@@ -1,1 +1,16 @@
+import rasterio
 
+# Access Vancouver datasets
+van_eth = rasterio.open('/scratch/arbmarta/ETH/Vancouver ETH.tif')
+van_lidar = rasterio.open('/scratch/arbmarta/LiDAR/Vancouver LiDAR.tif')
+van_meta = rasterio.open('/scratch/arbmarta/Meta/Vancouver Meta.tif')
+
+# Access Winnipeg datasets
+win_eth = rasterio.open('/scratch/arbmarta/ETH/Winnipeg ETH.tif')
+win_lidar = rasterio.open('/scratch/arbmarta/LiDAR/Winnipeg LiDAR.tif')
+win_meta = rasterio.open('/scratch/arbmarta/Meta/Winnipeg Meta.tif')
+
+# Access Ottawa datasets
+ott_eth = rasterio.open('/scratch/arbmarta/ETH/Ottawa ETH.tif')
+ott_lidar = rasterio.open('/scratch/arbmarta/LiDAR/Ottawa LiDAR.tif')
+ott_meta = rasterio.open('/scratch/arbmarta/Meta/Ottawa Meta.tif')
