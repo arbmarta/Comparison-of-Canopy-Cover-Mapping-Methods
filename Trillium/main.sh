26743a5ef126cf6148aff5e20bc7873db29bbf09
Alexander J. F. Martin <128729369+arbmarta@users.noreply.github.com>
2025-08-11 21:08:11 -0500

    Update main.sh

--- Trillium/main.sh
+++ Trillium/main.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=6
-#SBATCH --time=01:30:00
+#SBATCH --time=00:15:00
 #SBATCH --job-name=canopy_poly_split
 #SBATCH --output=/scratch/arbmarta/canopy_poly_split%j.out
 #SBATCH --error=/scratch/arbmarta/canopy_poly_split%j.err
