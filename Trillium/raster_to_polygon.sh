8235d7406dd2c96812d5c4f07b9942b1c0ca1fe0
Alexander J. F. Martin <128729369+arbmarta@users.noreply.github.com>
2025-08-12 10:59:30 -0500

    Update raster_to_polygon.sh

--- Trillium/raster_to_polygon.sh
+++ Trillium/raster_to_polygon.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
-#SBATCH --time=00:15:00
+#SBATCH --cpus-per-task=9
+#SBATCH --time=01:30:00
 #SBATCH --job-name=raster_to_polygon
-#SBATCH --output=/scratch/arbmarta/merge_eth_%j.out
+#SBATCH --output=/scratch/arbmarta/raster_to_polygon_%j.out
 
 source /home/arbmarta/.virtualenvs/myenv/bin/activate
 python /scratch/arbmarta/raster_to_polygon.py
