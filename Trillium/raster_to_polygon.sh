9d5deb8ab73e904c1c08b7adeb8e5ee40ac594b6
Alexander J. F. Martin <128729369+arbmarta@users.noreply.github.com>
2025-08-12 23:28:34 -0500

    Update raster_to_polygon.sh

--- Trillium/raster_to_polygon.sh
+++ Trillium/raster_to_polygon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=9
+#SBATCH --cpus-per-task=6
 #SBATCH --time=01:30:00
 #SBATCH --job-name=raster_to_polygon
 #SBATCH --output=/scratch/arbmarta/raster_to_polygon_%j.out
