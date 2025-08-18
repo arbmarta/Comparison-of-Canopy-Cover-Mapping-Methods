3e57074740f27d4755673755e65964483e11fa54
Alexander J. F. Martin <128729369+arbmarta@users.noreply.github.com>
2025-08-18 17:19:42 -0500

    Update canopy_models.sh

--- Trillium/canopy_models.sh
+++ Trillium/canopy_models.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=3
-#SBATCH --time=03:00:00
+#SBATCH --cpus-per-task=192
+#SBATCH --time=03:30:00
 #SBATCH --job-name=canopy_models
 #SBATCH --output=/scratch/arbmarta/canopy_models.out
 #SBATCH --error=/scratch/arbmarta/canopy_models.err
