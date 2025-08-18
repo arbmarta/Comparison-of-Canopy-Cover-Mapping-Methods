c7a63d448d30a5a21e3059ece76675a408ae49b3
Alexander J. F. Martin <128729369+arbmarta@users.noreply.github.com>
2025-08-18 17:45:33 -0500

    Update canopy_models.sh

--- Trillium/canopy_models.sh
+++ Trillium/canopy_models.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=192
 #SBATCH --time=03:30:00
 #SBATCH --job-name=canopy_models
 #SBATCH --output=/scratch/arbmarta/canopy_models.out
 #SBATCH --error=/scratch/arbmarta/canopy_models.err
 
-# Run your Python script
+# Activate virtual environment
 source /home/arbmarta/.virtualenvs/myenv/bin/activate
+
+# Run canopy modeling script
 python /scratch/arbmarta/canopy_models.py
