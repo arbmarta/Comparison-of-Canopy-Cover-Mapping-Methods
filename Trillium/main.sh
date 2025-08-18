66b29f45bc2cbba9b010c13ec6d72f567d6bd4fe
Alexander J. F. Martin <128729369+arbmarta@users.noreply.github.com>
2025-08-18 18:15:13 -0500

    Update main.sh

--- Trillium/main.sh
+++ Trillium/main.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=192
 #SBATCH --time=02:00:00
 #SBATCH --job-name=main
-#SBATCH --output=/scratch/arbmarta/main_%j.out
-#SBATCH --error=/scratch/arbmarta/main_%j.err
+#SBATCH --output=/scratch/arbmarta/main.out
+#SBATCH --error=/scratch/arbmarta/main.err
 
 # Run your Python script
 source /home/arbmarta/.virtualenvs/myenv/bin/activate
