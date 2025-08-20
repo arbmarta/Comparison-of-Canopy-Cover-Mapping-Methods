865aa7386e7c590e8b27d9960ca40238623f2d95
Alexander J. F. Martin <128729369+arbmarta@users.noreply.github.com>
2025-08-19 22:28:56 -0500

    Update buildings.py

--- Trillium/buildings.py
+++ Trillium/buildings.py
@@ -1,1 +1,21 @@
+import os
+import numpy as np
+import geopandas as gpd
+import rasterio
+from rasterio.mask import mask
+from rasterio.features import shapes
+from shapely.geometry import shape
+from multiprocessing import Pool
+import pandas as pd
+from tqdm import tqdm
+
+# Input boundaries
+van_bayan = gpd.read_file('/scratch/arbmarta/Trinity/Vancouver/TVAN.shp').to_crs("EPSG:32610")
+wpg_bayan = gpd.read_file('/scratch/arbmarta/Trinity/Winnipeg/TWPG.shp').to_crs("EPSG:32614")
+ott_bayan = gpd.read_file('/scratch/arbmarta/Trinity/Ottawa/TOTT.shp').to_crs("EPSG:32618")
+
+# Import buildings
+van_buildings = gpd.read_file('/scratch/arbmarta/Buildings/Vancouver Buildings.fgb').to_crs("EPSG:32610")
+wpg_buildings = gpd.read_file('/scratch/arbmarta/Buildings/Winnipeg Buildings.shp').to_crs("EPSG:32614")
+ott_buildings = gpd.read_file('/scratch/arbmarta/Buildings/Ottawa Buildings.shp').to_crs("EPSG:32618")
 
