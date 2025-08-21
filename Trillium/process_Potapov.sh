ab946261256f58672beaa72d448d5f65fcdb80b6
Alexander J. F. Martin <128729369+arbmarta@users.noreply.github.com>
2025-08-21 16:44:47 -0500

    Update process_Potapov.sh

--- Trillium/process_Potapov.sh
+++ Trillium/process_Potapov.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=00:15:00
+#SBATCH --job-name=process_Potapov
+#SBATCH --error=/scratch/arbmarta/process_Potapov.err
+#SBATCH --output=/scratch/arbmarta/process_Potapov.out
 
+source /home/arbmarta/.virtualenvs/myenv/bin/activate
+python /scratch/arbmarta/process_Potapov.py
