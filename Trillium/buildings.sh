b08409aec3f52c9f74aad5195a3e0340d154409b
Alexander J. F. Martin <128729369+arbmarta@users.noreply.github.com>
2025-08-23 15:04:29 -0500

    Update buildings.sh

--- Trillium/buildings.sh
+++ Trillium/buildings.sh
@@ -2,13 +2,13 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=192
-#SBATCH --time=00:30:00
+#SBATCH --time=00:45:00
 #SBATCH --job-name=buildings
-#SBATCH --output=/scratch/arbmarta/buildings.out
-#SBATCH --error=/scratch/arbmarta/buildings.err
+#SBATCH --output=/scratch/arbmarta/Outputs/Out/buildings.out
+#SBATCH --error=/scratch/arbmarta/Outputs/Err/buildings.err
 
 # Activate virtual environment
 source /home/arbmarta/.virtualenvs/myenv/bin/activate
 
 # Run canopy modeling script
-python /scratch/arbmarta/buildings.py
+python /scratch/arbmarta/Code/buildings.py
